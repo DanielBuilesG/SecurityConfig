42cdc8bef98c9ce563fc0ab696d7d404fe0eee6f
DanielBuilesG <daniel.builes99@gmail.com>
2023-07-18 12:13:11 -0500

    first commit

---
+++ src/main/java/com/mycompany/seguridadprimerdemo/SeguridadPrimerDemo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.seguridadprimerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author daniel.builes
+ */
+
+@SpringBootApplication
+public class SeguridadPrimerDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeguridadPrimerDemo.class, args);
+    }
+}
